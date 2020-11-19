6767a1b4f20bf6fb08915954e4ef489e5cc741b9
alexis <alexiscamus60@gmail.com>
2020-11-19 18:26:12 +0100

    Things are shaping well

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,111 @@
-import { API } from 'homebridge';
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service
+} from "homebridge";
+import {Somfy} from "./lib/Somfy";
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform'; 
+let hap: HAP;
 
-/**
- * This method registers the platform with Homebridge
+/*
+ * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  hap = api.hap;
+  api.registerAccessory("SomfyProtect", SomfyProtect);
 };
+
+class SomfyProtect implements AccessoryPlugin {
+
+  private readonly logger: Logging;
+  private readonly name: string;
+  private readonly somfy: Somfy;
+  private readonly siteId: string = "NxdSF9c2v5MxkeW5sqJCosXTTIT70xoS";
+  private state: number = hap.Characteristic.SecuritySystemCurrentState.DISARMED;
+
+  private readonly informationService: Service;
+  private readonly securitySystemService: Service;
+
+  constructor(logger: Logging, config: AccessoryConfig, api: API) {
+    this.logger = logger;
+    this.name = config.name;
+    this.somfy = new Somfy(logger, {
+      username: config.username as string,
+      password: config.password as string
+    });
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Somfy")
+      .setCharacteristic(hap.Characteristic.Model, this.name);
+
+    this.securitySystemService = new hap.Service.SecuritySystem(this.name);
+    this.securitySystemService.getCharacteristic(hap.Characteristic.SecuritySystemCurrentState)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        this.logger.info("Current state of security system was requested, requesting somfy");
+
+        this.somfy.getSite(this.siteId).then(result => {
+          const value = somfySecurityLevelToSecuritySystemState(result?.data.security_level);
+          this.logger.info("Got current state from somfy, returning: " + value);
+          callback(undefined, value);
+        });
+
+        // this.logger.info("Current state of security system was requested, returning: " + this.state);
+        // callback(undefined, this.state);
+      });
+
+    this.securitySystemService.getCharacteristic(hap.Characteristic.SecuritySystemTargetState)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        this.logger.info("Target state of security system was requested, requesting somfy");
+
+        this.somfy.getSite(this.siteId).then(result => {
+          const value = somfySecurityLevelToSecuritySystemState(result?.data.security_level);
+          this.logger.info("Got current state from somfy, returning: " + value);
+          callback(undefined, value);
+        });
+
+        // this.logger.info("Target state of security system was requested, returning: " + this.state);
+        // callback(undefined, this.state);
+      })
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        this.logger.info("Target state was set to: " + value);
+        this.state = value as number;
+        callback(undefined, this.state);
+        this.securitySystemService
+          .getCharacteristic(hap.Characteristic.SecuritySystemCurrentState)
+          .updateValue(value);
+      });
+
+    this.logger.info("Security system finished initializing!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [
+      this.informationService,
+      this.securitySystemService
+    ];
+  }
+
+}
+
+function somfySecurityLevelToSecuritySystemState(securityLevel: string): number {
+  switch (securityLevel) {
+    case "armed":
+      return hap.Characteristic.SecuritySystemCurrentState.AWAY_ARM;
+    case "partial":
+      return hap.Characteristic.SecuritySystemCurrentState.NIGHT_ARM;
+    case "disarmed":
+    default:
+      return hap.Characteristic.SecuritySystemCurrentState.DISARMED;
+  }
+}
---
+++ src/lib/Somfy.ts
@@ -0,0 +1,162 @@
+import {readFileSync, writeFileSync} from "fs";
+import axios from "axios";
+import {Logging} from "homebridge";
+
+const CLIENT_ID = "84eddf48-2b8e-11e5-b2a5-124cfab25595_475buqrf8v8kgwoo4gow08gkkc0ck80488wo44s8o48sg84k40";
+const CLIENT_SECRET = "4dsqfntieu0wckwwo40kw848gw4o0c8k4owc80k4go0cs0k844";
+
+export interface Config {
+  username: string;
+  password: string;
+}
+
+export class Somfy {
+  private config: Config;
+  private token: any;
+  private baseUrl = "https://api.myfox.io/v3";
+  private logger: Logging;
+
+  constructor(logger: Logging, config: Config) {
+    this.logger = logger;
+    this.config = config;
+
+    axios.interceptors.request.use(config => {
+      config.headers["request-startTime"] = process.hrtime();
+      config.headers["Content-Type"] = "application/json";
+      config.headers["Accept-Encoding"] = "gzip";
+      return config;
+    });
+
+    axios.interceptors.response.use(response => {
+      const start = response.config.headers["request-startTime"];
+      const end = process.hrtime(start);
+      const duration = Math.round((end[0] * 1000) + (end[1] / 1000000));
+      this.logger.info(`${response.config.url} : ${duration} ms`);
+      response.headers["request-duration"] = duration;
+      return response;
+    });
+  }
+
+  private async getNewToken() {
+    const token = await axios.post("https://sso.myfox.io/oauth/oauth/v2/token", {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      username: this.config.username,
+      password: this.config.password,
+      grant_type: "password"
+    });
+    this.token = token.data;
+    this.token.issuance = new Date().getTime();
+    writeFileSync("token.json", JSON.stringify(this.token));
+    return this.token;
+  }
+
+  private async getRefreshToken(refreshToken: string) {
+    const token = await axios.post("https://sso.myfox.io/oauth/oauth/v2/token", {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      refresh_token: refreshToken,
+      grant_type: "refresh_token"
+    });
+    this.token = token.data;
+    this.token.issuance = new Date().getTime();
+    writeFileSync("token.json", JSON.stringify(this.token));
+    return this.token;
+  }
+
+  private hasExpired() {
+    return this.token.issuance + this.token.expires_in - 1000 < new Date().getTime();
+  }
+
+  async updateToken() {
+    if (!!this.token && !this.hasExpired()) {
+      return this.token;
+    } else if (this.token) {
+      try {
+        this.token = await this.getRefreshToken(this.token.refresh_token);
+        return this.token;
+      } catch (error) {
+        this.logger.error(error.message);
+        this.logger.error("Need authorization request!");
+        return error.message;
+      }
+    } else {
+      try {
+        const data = readFileSync("token.json");
+        this.logger.warn("File token exist");
+        this.token = JSON.parse(data.toString());
+        if (this.hasExpired()) {
+          try {
+            this.token = await this.getRefreshToken(this.token.refresh_token);
+            writeFileSync("token.json", JSON.stringify(this.token));
+          } catch (error) {
+            this.logger.error(error.message);
+            this.logger.error("Need authorization request!");
+            return error.message;
+          }
+        }
+        this.logger.info("Return token");
+        return this.token;
+      } catch (error) {
+        try {
+          this.token = this.getNewToken();
+          return this.token;
+        } catch (e) {
+          this.logger.error(e);
+          this.logger.error(error.message);
+          this.logger.error("Need authorization request!");
+        }
+      }
+    }
+  }
+
+  async getALLSites() {
+    const token = await this.updateToken();
+    const options = {headers: {"Authorization": `Bearer ${token.access_token}`}};
+    try {
+      return await axios.get(`${this.baseUrl}/site`, options);
+    } catch (error) {
+      this.logger.error(error.message);
+    }
+  }
+
+  async getSite(siteId: string) {
+    const token = await this.updateToken();
+    const options = {headers: {"Authorization": `Bearer ${token.access_token}`}};
+    try {
+      return await axios.get(`${this.baseUrl}/site/${siteId}`, options);
+    } catch (error) {
+      this.logger.error(error.message);
+    }
+  }
+
+  async getDevicesFromSiteId(siteId: string) {
+    const token = await this.updateToken();
+    const options = {headers: {"Authorization": `Bearer ${token.access_token}`}};
+    try {
+      return await axios.get(`${this.baseUrl}/site/${siteId}/device`, options);
+    } catch (error) {
+      this.logger.error(error.message);
+    }
+  }
+
+  async getDevice(siteId: string, deviceId: string) {
+    const token = await this.updateToken();
+    const options = {headers: {"Authorization": `Bearer ${token.access_token}`}};
+    try {
+      return await axios.get(`${this.baseUrl}/site/${siteId}/device/${deviceId}`, options);
+    } catch (error) {
+      this.logger.error(error.message);
+    }
+  }
+
+  async setSecurityLevel(siteId: string, status: "disarmed" | "armed" | "partial") {
+    const token = await this.updateToken();
+    const options = {headers: {"Authorization": `Bearer ${token.access_token}`}};
+    try {
+      return await axios.put(`${this.baseUrl}/site/${siteId}/security`, {status}, options);
+    } catch (error) {
+      this.logger.error(error.message, error);
+    }
+  }
+}
